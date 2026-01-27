e757ae97d6c1b3c7443b9a71ca7b761badf94ad1
Oswaldoramirezb <oswaldo.ramirezbau@gmail.com>
2026-01-26 23:58:18 -0600

    Agrega archivo de prueba PHP para practicar comandos de GitHub

---
+++ cherry-pick/prueba-github.php
@@ -0,0 +1,10 @@
+<?php
+// Archivo de prueba para comandos de GitHub con PHP
+
+function saludo($nombre) {
+    return "Hola, $nombre!";
+}
+
+echo saludo("GitHub");
+
+// Modifica este archivo y usa los comandos de Git para practicar.
