e464974515c88caf66306eb517eccd9ddbfad308
Oswaldoramirezb <oswaldo.ramirezbau@gmail.com>
2026-01-27 23:36:04 -0600

    Agrega archivos de prueba para tag, cherry-pick, log, reflog y blame

---
+++ Ejercicios HTML/prueba-log-reflog.md
@@ -0,0 +1,8 @@
+# Archivo de prueba para git log y reflog
+
+- Practica los comandos:
+  - git log
+  - git log --oneline --graph
+  - git reflog
+
+Haz cambios y observa el historial de commits y referencias.
---
+++ cherry-pick/prueba-cherry-pick.js
@@ -0,0 +1,7 @@
+// Archivo de prueba para git cherry-pick
+
+function cherryPickDemo() {
+  return 'Prueba de git cherry-pick';
+}
+
+// Haz cambios en este archivo en diferentes ramas y usa git cherry-pick para traer los commits.
